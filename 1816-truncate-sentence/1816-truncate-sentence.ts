a25cc177216f27a6b0cbb83fc5c547f907c872ea
kamal <kamaludinakbar0@gmail.com>
2022-11-28 20:49:33 +0700

    Time: 94 ms (64.29%), Space: 44.1 MB (55.36%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.ts
+++ 1816-truncate-sentence/1816-truncate-sentence.ts
@@ -1,20 +1,3 @@
 function truncateSentence(s: string, k: number): string {
-    let cnt: number = 0;
-    const n: number = s.length;
-    let words: string = '';
-    let word: string = '';
-    for(let i = 0; i < n; i++) {
-        if (cnt == k) break;
-        if (s[i] == ' ') {
-            cnt++;
-            words += word + (cnt < k ? ' ': '');
-            word = '';
-        } else {
-            word += s[i];
-        }
-    };
-    if (word.length > 0) {
-        words += word;
-    }
-    return words;
+    return s.split(' ').slice(0, k).join(' ');
 };
